d295be7e9c8d3aa0d3c936fb0ac5e4c447ca6d6a
yasserelj <jariysser@gmail.com>
2022-07-04 11:44:59 +0100

    API implementation

---
+++ weatherAPI.py
@@ -0,0 +1,25 @@
+import requests
+from datetime import datetime
+
+key = '52c457badebc350feaaae7d514fa8248'
+
+now = datetime.now()
+
+def weatherAPI(city):
+
+    URL = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+key
+
+    response = requests.get(URL)
+
+    name = response.json()['name']
+
+    temp = int(response.json()['main']['temp'] - 273.15)
+
+    status = response.json()['weather'][0]['main']
+
+    humidity = response.json()['main']['humidity']
+
+    date = now.strftime("%d/%m/%Y %H:%M")
+    
+
+    return temp, status, humidity, date,name
